66c66ac7f1c2ef3ec00ad0d9ada6c6e143de64bf
dannoetc <dan.nelson@outlook.com>
2025-06-20 15:02:30 -0400

    Update Remediate-SecureBoot.ps1

--- SecureBoot/Remediate-SecureBoot.ps1
+++ SecureBoot/Remediate-SecureBoot.ps1
@@ -1,35 +1,22 @@
-#Enable SecureBoot
-
+# Enable SecureBoot
 $ErrorActionPreference = "Ignore"
-
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
 if (-not(Get-PackageProvider -name nuget)) {
-
 Install-PackageProvider -Name NuGet -Confirm:$false
-
 Start-Sleep -Seconds 60
-
 }
 
 if (-not(Get-InstalledModule -Name DellBIOSProvider)) {
-
 Install-Module -Name DellBIOSProvider -Confirm:$false
-
 Start-Sleep -Seconds 60
-
 }
 
 Import-Module DellBIOSProvider -force
 
 #Enable SecureBoot
-
 Start-Sleep -Seconds 60
-
 if (get-Item -Path DellSmbios:\SecureBoot\SecureBoot | select currentvalue) {
-
 Set-Item -Path DellSmbios:\SecureBoot\SecureBoot "Enabled"
-
 }
 
 exit
