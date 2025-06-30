6742b22d9276124111d9a2e2ab1a617d11b1a4ff
dannoetc <dan.nelson@outlook.com>
2025-06-30 16:33:27 -0400

    added IntuneBaseline, unattended reset customization,

---
+++ UnattendedReset/EnableCustomizations.cmd
@@ -0,0 +1,10 @@
+rem EnableCustomizations.cmd
+
+rem Set the variable %TARGETOS%      (Typically this is C:\Windows)
+for /F "tokens=1,2,3 delims= " %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RecoveryEnvironment" /v TargetOS') DO SET TARGETOS=%%C
+
+rem Set the variable %TARGETOSDRIVE% (Typically this is C:)
+for /F "tokens=1 delims=\" %%A in ('Echo %TARGETOS%') DO SET TARGETOSDRIVE=%%A
+
+rem Add back Windows settings, Start menu, and OOBE.xml customizations
+copy "%TARGETOSDRIVE%\Recovery\OEM\Unattend.xml" "%TARGETOS%\Panther\Unattend.xml" /y
